3f09921b544fc5250ce2f27cfd2a3a15f5776530
DkNabeel <143883787+DkNabeel@users.noreply.github.com>
2026-04-09 13:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ import os
 API_ID = int(os.getenv("29489981"))
 API_HASH = os.getenv("d06d320353dd369c577b33be893c622b")
 BOT_TOKEN = os.getenv("8083538429:AAG49IcZunYqVyuqz_7S1ynKOvbRUR38OI4")
-MONGO_URI = os.getenv("
-
-mongodb+srv://Nabeel: NPGzcTGYiqszW0j7y @cluster0.g5xiukh.mongodb.net/?appName=Cluster0")
+MONGO_URI = os.getenv("mongodb+srv://Nabeel: NPGzcTGYiqszW0j7y @cluster0.g5xiukh.mongodb.net/?appName=Cluster0")
 
 # ===== BOT INFO =====
 BOT_NAME = "DK Movie Bot 🎬"
